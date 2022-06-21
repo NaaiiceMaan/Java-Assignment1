2609db032bbdb04754372f88acb0887c6e057da4
alexander.fernandez@skillassure.com <alexander.fernandez@skillassure.com>
2022-06-21 17:16:08 +0530

    JavaAssignment1 comitted

--- JavaAssigment1/src/test/java/com/capgemini/javaAssignment1/JavaAssignment1.java
+++ JavaAssigment1/src/test/java/com/capgemini/javaAssignment1/JavaAssignment1.java
@@ -1,9 +1,10 @@
 package com.capgemini.javaAssignment1;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 
 public class JavaAssignment1 {
@@ -11,25 +12,23 @@ public class JavaAssignment1 {
 	public static void main(String[] args) throws Exception {
 		int count=0;
 		String[] stringToSearch;
-		List<String> listOfSrings = new ArrayList<String>();
 		Scanner in = new Scanner(new File(".\\src\\test\\resources\\Datafiles\\SearchKeyword.csv"));
 		do {
-	    stringToSearch=in.nextLine().split(", ");
+			stringToSearch=in.nextLine().split(", ");
 		}while(in.hasNext());
-		Scanner ni = new Scanner(new File(".\\src\\test\\resources\\Datafiles\\AlexanderResume.txt"));
-
-	    for(;ni.hasNext();){
-	        listOfSrings.add(ni.next());
-	    }
-	    for(int i=0;i<stringToSearch.length;i++) {
-			    if(listOfSrings.contains(stringToSearch[i])){
-			        count++;
-			    }
-	    }
-	    System.out.println(count);
-	    float perc = ((float)count/stringToSearch.length)*100;
-	    System.out.println("Percentage of matches : "+perc+"%");
-	    
+		File file = new File(".\\src\\test\\resources\\Datafiles\\Alexander's_Resume.pdf");
+		PDDocument doc= PDDocument.load(file);
+		PDFTextStripper pdfTextStripper =new PDFTextStripper();
+		String resumeText=pdfTextStripper.getText(doc);
+		for(int i=0;i<stringToSearch.length;i++) {
+			if(resumeText.contains(stringToSearch[i])){
+				count++;
+			}
+		}
+		System.out.println(count);
+		float perc = ((float)count/stringToSearch.length)*100;
+		System.out.println("Percentage of matches : "+perc+"%");
+		// extractor.close();
 	}
 
 }
